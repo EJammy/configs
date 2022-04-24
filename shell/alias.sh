8c3bee3d6dffa3dcf0c739ae86faee77cfad074d
connor <connorc0627@gmail.com>
2022-04-24 20:37:23 +0800

    [shell] single function for quick commit and push

--- shell/alias.sh
+++ shell/alias.sh
@@ -6,6 +6,19 @@ alias sudo='sudo '
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
+autocommit()
+{
+	git status
+	read -p "Commit and push changes? [y/n]: " -n 1 -r
+	echo    # (optional) move to a new line
+
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		git add . && git commit -am "Auto commit on $(date --utc +'%c')" && git push
+	else
+		echo "No action"
+	fi
+}
+
 
 mkcd()
 {
